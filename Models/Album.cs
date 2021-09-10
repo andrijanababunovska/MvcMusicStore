2ebdc0d3be50fe494fd7dcc4d406f5f7daef0cb6
Andrijana Babunovska <andrijana.babunovska94@gmail.com>
2021-09-10 20:07:27 +0200

    Add domain model, contoller, view classes

---
+++ Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public int ArtistId { get; set; }
+        public int GenreId { get; set; }
+
+        // navigational property
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
---
+++ Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer : CreateDatabaseIfNotExists<MusicStoreDb>
+    {
+        protected override void Seed(MusicStoreDb context)
+        {
+            // Artists
+            context.Artists.Add(new Artist { ArtistId = 1, Name = "Madonna" });
+            context.Artists.Add(new Artist { ArtistId = 2, Name = "John Lennon" });
+            context.Artists.Add(new Artist { ArtistId = 3, Name = "Michael Jackson" });
+            context.Artists.Add(new Artist { ArtistId = 4, Name = "Prince" });
+
+            // Genres
+            context.Genres.Add(new Genre { GenreId = 1, Name = "Rock" });
+            context.Genres.Add(new Genre { GenreId = 2, Name = "Blues" });
+            context.Genres.Add(new Genre { GenreId = 3, Name = "Pop" });
+            context.Genres.Add(new Genre { GenreId = 4, Name = "Hip hop" });
+
+            // Albums
+            context.Albums.Add(new Album
+            {
+                AlbumId = 1,
+                Title = "Thriller",
+                Artist = new Artist { Name = "Michael Jackson" },
+                Price = 49.99M,
+                Genre = new Genre { Name = "Pop" }
+            });
+
+            base.Seed(context);
+        }
+
+    }
+}
